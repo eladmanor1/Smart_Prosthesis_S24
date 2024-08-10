a3ac9b93dcaea39b8646fcb00720678155c6c243
ofirtab <ofirt@campus.technion.ac.il>
2024-08-10 16:08:01 +0300

    Added wifi client example to understand how to communicate with the hand and updated the yaml file

--- WIFI_client/WIFI_client.ino
+++ WIFI_client/WIFI_client.ino
@@ -3,12 +3,11 @@
 #include <Base64.h>
 
 const char* ssid = "Smart_Prosthesis_c_elad";       
-const char* password = "your_PASSWORD";      
+const char* password = "your_PASSWORD";
+// The default IP within the access point to communicate with the hand is 192.168.4.1  
 const char* serverName = "http://192.168.4.1/sensor_data"; 
 
-void setup() {
-  Serial.begin(115200);
-  delay(7000);
+void connect_to_wifi() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -17,21 +16,24 @@ void setup() {
   Serial.println("Connected to WiFi");
 }
 
-uint8_t value = 0;
-void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin(serverName);
+void setup() {
+  Serial.begin(115200);
+  delay(7000);
+  connect_to_wifi();
+}
 
-    uint8_t id = 1;
-    uint8_t payload[2] = {id, value};
-    value+=1;
+// This function adapts the payload for the server.
+// When sending a payload to the hand from a sensor, please use this function.
+void send_sensor_values(uint8_t *payload, int payload_len) {
     String payloadStr = "";
-    for (size_t i = 0; i < sizeof(payload); i++) {
+    for (size_t i = 0; i < payload_len; i++) {
       if (i > 0) payloadStr += ",";
-      Serial.println(payload[i]);
+      Serial.print(payload[i]);
       payloadStr += String(payload[i]);
     }
+
+    HTTPClient http;
+    http.begin(serverName);
     http.addHeader("Content-Type", "application/octet-stream");
     int httpResponseCode = http.POST(payloadStr);
 
@@ -44,11 +46,23 @@ void loop() {
       Serial.println(httpResponseCode);
     }
     http.end();
+}
+
+// In this example, we send a sensor value every second, with sensor id = 1.
+uint8_t value = 0;
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    uint8_t id = 1;
+    // First byte of the payload is always the sensor id, followed by the sensor values.
+    uint8_t payload[2] = {id, value};
+    value++;
+    send_sensor_values(payload, sizeof(payload));
   } else {
-    Serial.println("WiFi Disconnected");
+    Serial.println("WiFi Disconnected. Attempting to reconnect...");
+    connect_to_wifi();
   }
   delay(1000); // Send data every second
-  if(value > 20){
-    value=0;
+  if (value > 20) {
+    value = 0;
   }
 }
